ac347cb3b8e3ef3740fd877a1296d6f0985d84cd
Marcelo Archiza <archiza@hotmail.com>
2019-10-15 17:01:46 -0600

    added register button on main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Welcome to the <strong>Garage Sale</strong> app where you can buy and sell without worrying about scammers.</p>
     <p>
-      Please <router-link to="/login">Log In</router-link>
+      Please <router-link to="/login">Log In</router-link> or <router-link to="/register">Register</router-link>
     </p>
   </div>
 </template>
