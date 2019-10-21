703df7fa508be2e2ccfd4f9d7aee14ee1aad76f8
Marcelo Archiza <archiza@hotmail.com>
2019-10-21 10:24:01 -0600

    mocked verification

--- src/views/Verification.vue
+++ src/views/Verification.vue
@@ -13,12 +13,46 @@
       <el-table-column
         prop="studentID"
         label="StudentID"
-        width="180">
+        width="100">
       </el-table-column>
       <el-table-column
         prop="image"
         label=""
-        width="180">
+        width="90">
+        <template slot-scope="scope">
+        <el-button
+          @click.native.prevent="deleteRow(scope.$index, tableData)"
+          type="text"
+          size="small">
+          Download
+        </el-button>
+      </template>
+      </el-table-column>
+      <el-table-column
+        prop="approve"
+        label="Approve"
+        width="90">
+        <template slot-scope="scope">
+        <el-button
+          @click.native.prevent="deleteRow(scope.$index, tableData)"
+          type="text"
+          size="small">
+          Approve
+        </el-button>
+      </template>
+      </el-table-column>
+      <el-table-column
+        prop="reject"
+        label="Reject"
+        width="90">
+        <template slot-scope="scope">
+        <el-button
+          @click.native.prevent="deleteRow(scope.$index, tableData)"
+          type="text"
+          size="small">
+          Reject
+        </el-button>
+      </template>
       </el-table-column>
     </el-table>
 </template>
@@ -30,23 +64,19 @@
           tableData: [{
             date: '2019-10-19',
             name: 'Tom Smith',
-            studentID: '65-588-9886',
-            image: 'Download'
+            studentID: '65-588-9886'
           }, {
             date: '2019-10-19',
             name: 'Kaitlin Almeida',
-            address: '15-593-4785',
-            image: 'Download'
+            studentID: '15-593-4785'
           }, {
             date: '2019-10-20',
             name: 'Marcelo Archiza',
-            address: '74-555-9863',
-            image: 'Download'
+            studentID: '74-555-9863'
           }, {
             date: '2019-10-20',
             name: 'Jacob Cho',
-            studentID: '00-000-0000',
-            image: 'Download'
+            studentID: '00-000-0000'
           }]
         }
       }
