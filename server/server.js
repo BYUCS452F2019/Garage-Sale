a564241aa0339011fa4a65ca7e01a9919dd73633
Jacob Cho <jcho@qualtrics.com>
2019-10-23 22:39:33 -0600

    Mongo init

--- server/server.js
+++ server/server.js
@@ -1,65 +1,26 @@
-const express = require('express')
-const bodyParser = require("body-parser");
-const Accounts = require('./controllers/accounts')
-const cors = require('cors')
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('./_helpers/jwt');
+const errorHandler = require('./_helpers/error-handler');
 
-module.exports = async function (dbClient, port = 0) {
-  const app = express()
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-  const accounts = Accounts(dbClient)
-  
-  app.get('/', function(req, res) {
-    res.sendStatus(200)
-  });
-  app.put('/login', async (req, res) => {
-    // TODO
-    const { email, password } = req.body
-    const user = await accounts.login(email, password)
+// use JWT auth to secure the api
+app.use(jwt());
 
-    if (user) {
-      res.json(user)
-    } else {
-      res.sendStatus(400)
-    }
-  })
+// api routes
+app.use('/users', require('./users/users.controller'));
 
-  app.post('/change-password', async (req, res) => {
-    // const { newPassword, oldPassword } = req.body
-    // const user = req.user
-    // const changed = await accounts.changePassword(user.email, oldPassword, newPassword)
-    // if (changed) {
-    //   res.sendStatus(200)
-    // } else {
-    //   res.status(400).send('Incorrect email or password')
-    // }
-  })
-  
-  app.post('/register', async (req, res) => {
-    const { email, password } = req.body
-    const created = await accounts.create(email, password)
-    if (created) {
-      res.sendStatus(200)
-    } else {
-      res.sendStatus(400)
-    }
-  })
+// global error handler
+app.use(errorHandler);
 
-  return new Promise(function (resolve, reject) {
-    const listener = app.listen(port, function (err) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve({
-          port: listener.address().port,
-          stop: () => {
-            listener.close()
-            console.log('Server stopped')
-          }
-        })
-      }
-    })
-  })
-}
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
