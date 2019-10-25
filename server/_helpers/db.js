9423a43e049d6db8668381aeea8bd4c157ce3ecb
Jacob Cho <jcho@qualtrics.com>
2019-10-25 14:11:10 -0600

    Schema update

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -1,6 +1,7 @@
 const config = require('../config.json');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true,}).then(() => console.log('DB Connected!'))
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true,})
+.then(() => console.log('DB Connected!'))
 .catch(err => {
 console.log("DB Connection Error: ", err.message);
 });
--- server/users/user.model.js
+++ server/users/user.model.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, unique: true, required: true },
+    user_id: { type: String, unique: true, required: true },
+    password: { type: String, unique:false, required: true },
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    email: { type: String, required: true },
+    id_photo: { type: Buffer, contentType: String },
+    validated: { type: Boolean, required: true }
 });
 
 schema.set('toJSON', { virtuals: true });
