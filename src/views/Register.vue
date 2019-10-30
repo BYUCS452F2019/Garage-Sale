c8239951815b54a20972b64d9ef9104eedea5ac0
Franchesca Uy <franchesca_u97@yahoo.com>
2019-10-30 15:31:33 -0600

    changed the labels in the Register.vue page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -16,18 +16,18 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.firstName" required placeholder="Enter name"></b-form-input>
+      <b-form-group id="input-group-1" label="First Name:" label-for="input-1">
+        <b-form-input id="input-1" v-model="form.firstName" required placeholder="Enter first name"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.lastName" required placeholder="Enter name"></b-form-input>
+      <b-form-group id="input-group-2" label="Last Name:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.lastName" required placeholder="Enter last name"></b-form-input>
       </b-form-group>
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.user_id" required placeholder="Enter name"></b-form-input>
+      <b-form-group id="input-group-3" label="Student ID:" label-for="input-3">
+        <b-form-input id="input-3" v-model="form.user_id" required placeholder="Enter BYU student ID"></b-form-input>
       </b-form-group>
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.password" required placeholder="Enter name"></b-form-input>
+      <b-form-group id="input-group-4" label="Password:" label-for="input-4">
+        <b-form-input id="input-4" type="password" v-model="form.password" required placeholder="Enter password"></b-form-input>
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
