7ca62eefdaf4a836c0098365d4e2c372303dea5d
Douglas Uriona <duriona@gmail.com>
2019-10-31 17:16:18 -0600

    Simplified the profile page

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
-    <b-button v-b-modal.remove-item>Visible Modal for now</b-button>
-
     <b-modal id="remove-item" title="Remove Item" ok-title="Yes" cancel-title="No">
       <p class="my-4">Are you sure you want to remove this item for sale?</p>
     </b-modal>
-
-    <h2 style="display: inline-block">Profile</h2>
+    <h1 style="text-align:center">Profile</h1>
     <contact />
     <!-- <el-button style="float:right" @click="$router.push('/profile/edit')">Edit Profile</el-button> -->
     <h4 style="text-align:center">My Items for Sell</h4>
-    <b-table striped hover :items="items" :fields="fields">
-      <template v-slot:cell(remove)="row">
+    <b-table striped hover :items="items" :fields="data">
+      <!-- <template v-slot:cell(remove)>
         <b-button size="sm" class="mr-1">stuff</b-button>
-      </template>
+      </template>-->
     </b-table>
+    <!-- <b-button v-b-modal.remove-item>Visible Modal for now</b-button> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Contact from "./Contact.vue";
+import Contact from "./Contact";
 
 export default {
   components: {
@@ -28,13 +26,13 @@ export default {
   },
   data() {
     return {
-      fields: [
+      data: [
         { label: "Item Name", key: "item_name" },
         { label: "Description", key: "description" },
         { label: "Date Added", key: "date_added" },
         { label: "Price", key: "price" },
-        { label: "Area Code", key: "area_code" },
-        { label: "Remove", key: "remove" }
+        { label: "Area Code", key: "area_code" }
+        // { label: "Remove", key: "remove" }
       ],
       items: [
         {
--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,50 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.vue']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue?$/,
-                exclude: /(node_modules)/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.css$/i,
-                loader: 'css-loader',
-                options: {
-                    url: true,
-                },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue?$/,
+        exclude: /(node_modules)/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/i,
+        loader: 'css-loader',
+        options: {
+          url: true
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
             }
+          }
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:3000'
-        })
-    }
-}
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: 'http://localhost:3000'
+    })
+  }
+};
