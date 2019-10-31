b1db75299632b90b8ec72ac18693dc7ed56d28f8
Marcelo Archiza <archiza@hotmail.com>
2019-10-31 16:48:21 -0600

    Added login and register buttons

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -41,7 +41,7 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="submit" variant="primary">Submit</b-button> OR <router-link to="/login">Login</router-link>
     </b-form>
     <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
