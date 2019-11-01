d59caf92eea197e88180675479d1d7f92b8b8cb6
Douglas Uriona <duriona@gmail.com>
2019-10-31 21:44:10 -0600

    Got the items to appear in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,61 @@
 <template>
   <div>
-    <p>Welcome to the <strong>Garage Sale</strong> app where you can buy and sell without worrying about scammers.</p>
-    <p>
-      Please <router-link to="/login">Log In</router-link> or <router-link to="/register">Register</router-link>
-    </p>
+    <div v-if="!isLoggedIn">
+      <p>
+        Welcome to the
+        <strong>Garage Sale</strong> app where you can buy and sell without worrying about scammers.
+      </p>
+      <p>
+        Please
+        <router-link to="/login">Login</router-link>&#09; or
+        <router-link to="/register">Register</router-link>
+      </p>
+    </div>
+    <h1 style="text-align: center">Items for Sale</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="(name, index) in names" :key="index">{{name}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in allItems" :key="index">
+          <td v-for="(column, indexColumn) in columns" :key="indexColumn">{{item[column]}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      names: ["Item Name", "Description", "Date Added", "Price", "Area Code"],
+      columns: ["item_name", "item_descr", "date_added", "price", "area_code"]
+    };
+  },
+  created() {
+    this.getAllItems();
+    console.log(this.allItems);
+  },
+  computed: {
+    ...mapState("users", ["allItems"])
+  },
+  methods: {
+    ...mapActions("users", ["getAllItems"]),
+    isLoggedIn() {
+      return localStorage.getItem("user") != null;
+    },
+    loginUser(e) {
+      this.submitted = true;
+      // const { email, password } = this.form;
+      // if (email && password) {
+      this.login({ email: this.form.email, password: this.form.password });
+      // }
+    }
+  }
+};
+</script>
